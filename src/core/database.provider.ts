633d2f2acf05645d4089f05dd27b61d27beae39b
David <daveleo250@gmail.com>
2025-02-10 17:29:04 +0200

    env

--- src/core/database.provider.ts
+++ src/core/database.provider.ts
@@ -6,7 +6,7 @@ export const DatabaseProviders = TypeOrmModule.forRootAsync({
   useFactory: (configService: ConfigService) => ({
     type: configService.get('DB_TYPE') as any,
     host: configService.get<string>('DB_HOST')!,
-    port: parseInt(configService.get<string>('DB_PORT')!),
+    port: parseInt(configService.get<string>('DB_PORT')),
     username: configService.get<string>('DB_USERNAME')!,
     password: configService.get<string>('DB_PASSWORD')!,
     database: configService.get<string>('DB_DATABASE')!,
